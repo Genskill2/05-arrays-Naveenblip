5c03d880de0681eb3996b0ef39a2b1313097b900
Naveenblip <84712340+Naveenblip@users.noreply.github.com>
2021-06-16 08:02:44 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,88 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int num[], int s)
+{
+  int max= num[0];
+  for(int i=0;i<s;i++)
+  {
+    if(num[i]>max)
+    {
+      max= num[i];
+    }
+  }
+  return max;
+}
+
+int min(int num[], int s)
+{
+  int min= num[0];
+  for(int i=0;i<s;i++)
+  {
+    if(num[i]<min)
+    {
+      min= num[i];
+    }
+  }
+  return min;
+}
+
+float average(int num[], int s)
+{
+  float total = 0;
+  for(int i=0;i< s;i++)
+  {
+    sum += num[i];
+  }
+  return (sum/size);
+}
+
+int mode(int num[], int s)
+{
+  int c[s];
+  int index =0;
+  int max = -999999;
+  for(int i =0; i<size; i++)
+  {
+    for(int j=0; j< size; j++)
+    {
+      if num[i] == num [j]
+      {
+        c[i]++;
+      }
+    }
+  }
+  for(int k= 0;k<s;k++)
+  {
+    if(cnt[k] > max)
+    {
+      index=i;
+    }
+  }
+  return(num[index]);
+}
+
+int factors(int num; int prf[])
+{
+  int temp = num;
+  int i=0;
+  int pf[]={2,3,5,7,9,11,13,17,19};
+  int np = (int)(sizeof(pf)/sizeof(int));
+  for(;temp!=i;i++)
+  {
+    for(int j=0; j,np;j++)
+     {
+            if((temp%pf[j])==0)//check the divisibility with primes
+            {
+                prf[i]=pf[j];//store prime factors in pfact array
+                temp/=pf[j];
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+    return i;
+} 
+    
